cc161c06538be34c47a990f852bcb89e548d6873
Mickael Serge Zehren <mickael.zehren@gmail.com>
2018-09-29 10:12:08 +0200

    Correcting the pad "ghosting"

--- ADTOF/midiProxy.py
+++ ADTOF/midiProxy.py
@@ -3,18 +3,18 @@ import midi
 import json
 import os
 
+# Load notes dictionaries
 # when the 112 is played, the 100 is played too but shouldn't
 # I think it is an artefact from the pads used to tab the musics
-with open(os.path.join(os.path.dirname(__file__),"./conversionDictionnaries/PhaseShifterArtefacts.json"), 'r') as outfile:
-    PADARTEFACTS = {int(key):int(value) for key,value in json.load(outfile).items()}
+with open(os.path.join(os.path.dirname(__file__), "./conversionDictionnaries/PhaseShifterArtefacts.json"), 'r') as outfile:
+    PADARTEFACTS = {int(key): int(value) for key, value in json.load(outfile).items()}
 # see https://en.wikipedia.org/wiki/General_MIDI#Percussion for the full list of events
-with open(os.path.join(os.path.dirname(__file__),"./conversionDictionnaries/StandardMidiToReduced.json"), 'r') as outfile:
-    MIDIREDUCED = {int(key):int(value) for key,value in json.load(outfile).items()}
+with open(os.path.join(os.path.dirname(__file__), "./conversionDictionnaries/StandardMidiToReduced.json"), 'r') as outfile:
+    MIDIREDUCED = {int(key): int(value) for key, value in json.load(outfile).items()}
 # midi notes used by the game Phase Shifter
 # the controller doesn't have a precise representation of each drums
-with open(os.path.join(os.path.dirname(__file__),"./conversionDictionnaries/PhaseShifterMidiToStandard.json"), 'r') as outfile:
-    PHASEMIDI = {int(key):int(value) for key,value in json.load(outfile).items()}
-
+with open(os.path.join(os.path.dirname(__file__), "./conversionDictionnaries/PhaseShifterMidiToStandard.json"), 'r') as outfile:
+    PHASEMIDI = {int(key): int(value) for key, value in json.load(outfile).items()}
 
 
 def main(phaseShiftMidiPath, outputMidiPath):
@@ -26,33 +26,47 @@ def main(phaseShiftMidiPath, outputMidiPath):
 
     # Check if the format of the midi file is supported
     if pattern.format != 1 and pattern.resolution < 0:
-        print("ERROR midi format not implemented")
-        return
+        Exception("ERROR midi format not implemented")
 
-    # Change the pitch of each event to the standard one
-    for track in pattern[1:]:
+    # Change the note of each event to the standard midi notation
+    for i, track in enumerate(pattern):
+        # remove the first track which is the tempo track
+        if i == 0: continue
+
+        simultaneousEvents = []
         for event in track:
-            if event.name == "Note Off" or event.name == "Note On":
-                event.data = convertMidiEvent(event.data)
-                
+            simultaneousEvents.append(event)
+
+            if event.tick != 0:
+                convertSimultaneousEvents([localEvent for localEvent in simultaneousEvents if localEvent.name == "Note On"])
+                convertSimultaneousEvents([localEvent for localEvent in simultaneousEvents if localEvent.name == "Note Off"])
+                simultaneousEvents = []
+
+        # Remove empty events:
+        pattern[i] = [event for event in pattern[i] if (event.name != "Note On" and event.name != "Note Off") or event.data[0] != None]
+
     # Write the resulting file
     midi.write_midifile(outputMidiPath, pattern)
 
-def convertMidiEvent(notes):
-    """
-    TODO
-    """
-    # remove the events wich shouldn't be there
-    notes = [note for note in notes if not(note in PADARTEFACTS and PADARTEFACTS[note] in notes)]
-    
-    # Convert to standard midi events
-    notes = [PHASEMIDI[note] if note in PHASEMIDI else note for note in notes]
 
-    # Reduce the number of classes by merging drums
-    notes = [MIDIREDUCED[note] if note in MIDIREDUCED else note for note in notes]
+def convertSimultaneousEvents(events):
+    """
+    TODO : add documentation
+    """
+    allnotes = [event.data[0] for event in events]
 
-    # Remove duplicated events
-    notes = list(set(notes))
-    return notes
+    for event in events:
+        # remove the notes wich shouldn't be there
+        if event.data[0] in PADARTEFACTS and PADARTEFACTS[event.data[0]] in allnotes:
+            event.data[0] = None
+        # Convert to standard midi pitch
+        event.data[0] = PHASEMIDI[event.data[0]] if event.data[0] in PHASEMIDI else event.data[0]
+        # Reduce the number of pitches by converting to base classes
+        event.data[0] = MIDIREDUCED[event.data[0]] if event.data[0] in MIDIREDUCED else event.data[0]
+
+    # # remove duplicated notes
+    # return list(set(events))
+    return events
+
 
 main("notes.mid", 'result.mid')
