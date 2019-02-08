f6c8b8a3bdd4f75632d283b8447bf6a7def084b6
Mickael Serge Zehren <mickael.zehren@gmail.com>
2019-02-08 15:06:34 +0100

    the error is well computed on the first file

--- adtof/__init__.py
+++ adtof/__init__.py
@@ -1,1 +1,3 @@
 from . import converters, io
+
+
--- adtof/io/midoProxy.py
+++ adtof/io/midoProxy.py
@@ -3,17 +3,33 @@ import numpy as np
 from mido import MidiFile
 
 
+def lazy_property(fn):
+    '''Decorator that makes a property lazy-evaluated.
+    TODO change the location ?
+    '''
+    attr_name = '_lazy_' + fn.__name__
+
+    @property
+    def _lazy_property(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+
+    return _lazy_property
+
+
 class MidoProxy(MidiFile):
     """
     Encapsulating MIDI functionalities in this proxy class to open the possibility of supporting another midi library more easily.
     """
 
-    def getTempoEvent(self):
+    @lazy_property
+    def tempoEvents(self):
         """
         Return a list a tuples
         [(ticks, mpqn)]
         """
-        tempoEvents = [(0, 500000)] #Start with the default microseconds/beats, which is 120 bpm
+        tempoEvents = [(0, 500000)]  #Start with the default microseconds/beats, which is 120 bpm
         tickCursor = 0
         if self.type != 1:
             raise NotImplementedError()
@@ -22,47 +38,77 @@ class MidoProxy(MidiFile):
             tickCursor += tempoEvent.time
             if tempoEvent.type == 'set_tempo':
                 currentMpqn = tempoEvent.tempo
-                if tempoEvents[-1][0] == tickCursor: #Make sure there are no overlapping tempo events
+                if tempoEvents[-1][0] == tickCursor:  #Make sure there are no overlapping tempo events
                     tempoEvents[-1] = (tickCursor, currentMpqn)
                 else:
                     tempoEvents.append((tickCursor, currentMpqn))
 
         return tempoEvents
 
-    def getTicksToSecond(self, ticks, tempo):
+    def getTicksToSecond(self, ticks, start=0, tempo=0):
         """
         Transform a number of ticks with a tempo in microseconds per beat into seconds based on the resolution of the midi file
         """
-        beat = ticks / self.ticks_per_beat
+        end = ticks
+        delta = end - start
+        if not tempo: #get all the tempo changes occuring between the start and end locations. The resulting tempo is the weighted average
+            tempoEvents = self.tempoEvents
+            selectedTempo = [t for t in tempoEvents if t[0] > start and t[0] < end] #get the tempo changes during the event
+            tempo0 = [t[1] for t in tempoEvents if t[0] <= start][0] #get the tempo at the start location
+            Tempi = [tempo0] + [t[1] for t in selectedTempo]
+            weight = np.diff([start] + [t[0] for t in selectedTempo] + [end]) / delta #get the weighted average of all the tempi
+            tempo = np.sum(Tempi * weight)
+
+        beat = delta / self.ticks_per_beat #convert the ticks in beat and then seconds
         usPerBeats = beat * tempo
         return usPerBeats / 1000000
 
-    def getEventsPosition(self):
-        """
-        Compute the time in seconds of each event
-        """
-        tempoEvents = self.getTempoEvent()
+    def getSecondToTicks(self, second):
+        raise NotImplementedError()
 
-        for track in self.tracks[1:]:
+    def getOnsets(self):
+        """
+        Return a list a positions in seconds of the notes_on events
+        """
+        notesPositions = []
+        for track in self.tracks:
             tickCursor = 0
-            timeCursor = 0
-
             for event in track:
                 if event.time:
-                    start = tickCursor
-                    end = tickCursor + event.time
-                    selectedTempo =  [t for t in tempoEvents if t[0] > start and t[0] < end]  
+                    tickCursor += event.time
 
-                    tempo0 = [t[1] for t in tempoEvents if t[0] <= start][0]
-                    Tempi = [tempo0] + [t[1] for t in selectedTempo]
-                    weight = np.diff([start] + [t[0] for t in selectedTempo] + [end]) / event.time
+                    if event.type == "note_on" and event.velocity > 0:
+                        notesPositions.append(self.getTicksToSecond(tickCursor))
 
-                    timeCursor += self.getTicksToSecond(event.time, np.sum(Tempi * weight))
+        return notesPositions
 
-                tickCursor += event.time
-                #TODO find a better method than: event.time = timeCursor
-                
+    # def getEventsPosition(self):
+    #     """
+    #     Compute the time in seconds of each event
+    #     """
+    #     tempoEvents = self.getTempoEvent()
 
+    #     positionLookup = {}
+    #     for track in self.tracks[1:]:
+    #         tickCursor = 0
+    #         timeCursor = 0
+    #         for event in track:
+    #             if event.time:
+    #                 start = tickCursor
+    #                 end = tickCursor + event.time
+    #                 selectedTempo = [t for t in tempoEvents if t[0] > start and t[0] < end]
+
+    #                 tempo0 = [t[1] for t in tempoEvents if t[0] <= start][0]
+    #                 Tempi = [tempo0] + [t[1] for t in selectedTempo]
+    #                 weight = np.diff([start] + [t[0] for t in selectedTempo] + [end]) / event.time
+
+    #                 timeCursor += self.getTicksToSecond(event.time, np.sum(Tempi * weight))
+
+    #                 tickCursor += event.time
+    #                 #TODO find a better method than: event.time = timeCursor
+    #                 positionLookup[tickCursor] = timeCursor
+
+    #     return positionLookup
 
     def getTrackNames(self):
         """
@@ -70,5 +116,3 @@ class MidoProxy(MidiFile):
         """
         tracksNames = [[event.name for event in track if event.type == "track_name"] for track in self.tracks]
         return [names[0] if names else None for names in tracksNames]
-
-class timeValue(Int)
