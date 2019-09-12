772403eb4662be9ca9511ea392cd6e7e33e63308
Mickael Serge Zehren <mickael.zehren@gmail.com>
2019-09-12 17:29:04 +0200

    improve model loading

--- bin/trainModel.py
+++ bin/trainModel.py
@@ -25,22 +25,28 @@ def main():
     Entry point of the program
     Parse the arguments and call the conversion
     """
-    
     parser = argparse.ArgumentParser(description='todo')
     parser.add_argument('folderPath', type=str, help="Path.")
     args = parser.parse_args()
 
+    # Get the data
     dataset, dataset_test = Converter.convertAll(args.folderPath)
     dataset = dataset.batch(400).repeat()
     dataset_test = dataset_test.batch(400).repeat()
 
+    # Get the model
     model = RV1().createModel()
-
+    checkpoint_path = "models/rv1.ckpt"
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+    latest = tf.train.latest_checkpoint(checkpoint_dir)
+    if latest:
+        model.load_weights(latest)
+    
     log_dir = os.path.join("logs", "fit", "rv1.2") #datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     callbacks = [
         tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1, write_images=False, profile_batch=0),
         # tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=2, verbose=1),
-        tf.keras.callbacks.ModelCheckpoint("models/rv1", save_weights_only=True, load_weights_on_restart=True)
+        tf.keras.callbacks.ModelCheckpoint(checkpoint_path, save_weights_only=True, load_weights_on_restart=True)
     ]
     # model.load_weights(checkpoint_path, )
 
