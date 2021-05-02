625c69d6cf6ee9288a03358225972e2a46b887d3
mzehren <mickael.zehren@gmail.com>
2021-05-02 14:54:29 +0200

    renaming it c

--- adtof/model/model.py
+++ adtof/model/model.py
@@ -96,7 +96,7 @@ class Model(object):
                 "reduce_patience": 5,
                 "stopping_patience": 10,
             },
-            "crnn-ADTOF-log71b": {
+            "crnn-ADTOF-log71c": {
                 "labels": config.LABELS_5,
                 "classWeights": config.WEIGHTS_5 / 10,
                 "emptyWeight": 1,
